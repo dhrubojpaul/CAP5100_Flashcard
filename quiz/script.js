90ba418ef68fe511fd9ccc610350e34a4194d57c
Dhrubo Jyoti Paul <dhrubojpaul@gmail.com>
2020-03-22 23:39:56 -0400

    score

--- quiz/script.js
+++ quiz/script.js
@@ -22,6 +22,10 @@ var findObjectByID = function(asset, id){
     return object;
 }
 
+var play = function(id){
+    document.getElementById(id).play();
+}
+
 Vue.component("quiz", {
     template: `
     <v-card-text>
@@ -34,34 +38,35 @@ Vue.component("quiz", {
         <v-simple-table>
             <tbody>
                 <tr>
-                    <td>{{current.type==1 ? current.word.chn : current.word.eng}}</td>
+                    <td>{{current.type==1 ? current.word.chn+ ' (' + current.word.pinyin + ')' : current.word.eng}}</td>
                     <!--<td><img height="100px" :src="current.word.image"></img></td>-->
                     <td><audio controls autoplay :src="current.type == 1 ? current.word.chnaudio : current.word.engaudio"></audio></td>
                 </tr>
             </tbody>
         </v-simple-table>
 
-        <br/>
-        <h4>Options</h4>
-        <v-simple-table dense>
-            <tbody>
-                <v-radio-group v-model="values.selected" :readonly="(current.hint && !values.isHinted) || values.isSubmitted">
-                    <tr v-for="option in current.options">
-                        <td><v-radio :value=option.id></v-radio></td>
-                        <td>{{current.type == 1 ? option.eng : option.chn}}</td>
-                        <td><img :src="option.image" height=54px></img></td>
-                        <td><audio controls :src="current.type == 1 ? option.engaudio : option.chnaudio"></audio></td>
-                        <td v-if="(option.id == values.selected || option.id == current.word.id) && values.isSubmitted">
-                            {{current.word.id == option.id ? "Correct" : "Incorrect"}}
-                        </td>
-                    </tr>
-                </v-radio-group>
-            </tbody>
-        </v-simple-table>
-
         <div class="teal darken-2 text-center">
             <span class="white--text">{{instruction}}</span>
-        </div>
+        </div>  
+
+        <v-radio-group v-model="values.selected" :readonly="(current.hint && !values.isHinted) || values.isSubmitted">
+        <v-card class="d-flex justify-space-between mb-3" height=54px flat v-for="option in current.options">
+            <v-card class="pa-2" flat><v-radio :value=option.id></v-radio></v-card>
+            <v-card class="pa-2" flat>{{current.type == 1 ? option.eng : current.word.chn+ ' (' + current.word.pinyin + ')'}}</v-card>
+            <v-card class="pa-0"  flat v-if="current.type == 1"><img :src="option.image" height=54px></img></v-card>
+            <v-card flat>
+                <v-btn style="margin:0 !important" icon @click="play(option.id)"><v-icon>mdi-play</v-icon></v-btn>
+            </v-card>
+            <audio :src="current.type == 1 ? option.engaudio : option.chnaudio" :id="option.id"></audio>
+            <v-card class="pa-2" flat>
+                <p v-if="(option.id == values.selected || option.id == current.word.id) && values.isSubmitted">
+                    {{current.word.id == option.id ? "Correct" : "Incorrect"}}
+                </p>
+            </v-card>
+        </v-card>
+        </v-radio-group>
+
+        
 
         <v-btn block v-if="values.role==1 && values.selected && !values.isSubmitted && !values.isHinted" @click="assist">Assist</v-btn>
         <v-btn block v-if="values.role==2 && values.selected && !values.isSubmitted && values.isHinted" @click="submit">Submit</v-btn>
@@ -83,63 +88,63 @@ Vue.component("quiz", {
                 isHinted: undefined
             },
             asset: [
-                {id:1, chn:"苹果", eng:"apple", chnaudio: "./assets/apple_chnaudio.mp3", engaudio: "./assets/apple_engaudio.mp3", image: "./assets/apple_photo.png"},
-                {id:2, chn:"香蕉", eng:"banana", chnaudio: "./assets/banana_chnaudio.mp3", engaudio: "./assets/banana_engaudio.mp3", image: "./assets/banana_photo.png"},
-                {id:3, chn:"橙子", eng:"orange", chnaudio: "./assets/orange_chnaudio.mp3", engaudio: "./assets/orange_engaudio.mp3", image: "./assets/orange_photo.png"},
-                {id:4, chn:"草莓", eng:"strawberry", chnaudio: "./assets/strawberry_chnaudio.mp3", engaudio: "./assets/strawberry_engaudio.mp3", image: "./assets/strawberry_photo.png"},
-                {id:5, chn:"西瓜", eng:"watermelon", chnaudio: "./assets/watermelon_chnaudio.mp3", engaudio: "./assets/watermelon_engaudio.mp3", image: "./assets/watermelon_photo.png"},
-                {id:6, chn:"土豆", eng:"potato", chnaudio: "./assets/potato_chnaudio.mp3", engaudio: "./assets/potato_engaudio.mp3", image: "./assets/potato_photo.png"},
-                {id:7, chn:"黄瓜", eng:"cucumber", chnaudio: "./assets/cucumber_chnaudio.mp3", engaudio: "./assets/cucumber_engaudio.mp3", image: "./assets/cucumber_photo.png"},
-                {id:8, chn:"菜花", eng:"cauliflower", chnaudio: "./assets/cauliflower_chnaudio.mp3", engaudio: "./assets/cauliflower_engaudio.mp3", image: "./assets/cauliflower_photo.png"},
-                {id:9, chn:"生菜", eng:"lettuce", chnaudio: "./assets/lettuce_chnaudio.mp3", engaudio: "./assets/lettuce_engaudio.mp3", image: "./assets/lettuce_photo.png"},
-                {id:10, chn:"洋葱", eng:"onion", chnaudio: "./assets/onion_chnaudio.mp3", engaudio: "./assets/onion_engaudio.mp3", image: "./assets/onion_photo.png"},
+                {id:1, chn:"苹果", eng:"apple", pinyin: "píng guǒ", chnaudio: "./assets/apple_chnaudio.mp3", engaudio: "./assets/apple_engaudio.mp3", image: "./assets/apple_photo.png"},
+                {id:2, chn:"香蕉", eng:"banana", pinyin: "xiāng jiāo", chnaudio: "./assets/banana_chnaudio.mp3", engaudio: "./assets/banana_engaudio.mp3", image: "./assets/banana_photo.png"},
+                {id:3, chn:"橙子", eng:"orange", pinyin: "chéng zǐ", chnaudio: "./assets/orange_chnaudio.mp3", engaudio: "./assets/orange_engaudio.mp3", image: "./assets/orange_photo.png"},
+                {id:4, chn:"草莓", eng:"strawberry", pinyin: "cǎo méi", chnaudio: "./assets/strawberry_chnaudio.mp3", engaudio: "./assets/strawberry_engaudio.mp3", image: "./assets/strawberry_photo.png"},
+                {id:5, chn:"西瓜", eng:"watermelon", pinyin: "xī guā", chnaudio: "./assets/watermelon_chnaudio.mp3", engaudio: "./assets/watermelon_engaudio.mp3", image: "./assets/watermelon_photo.png"},
+                {id:6, chn:"土豆", eng:"potato", pinyin: "tǔ dòu", chnaudio: "./assets/potato_chnaudio.mp3", engaudio: "./assets/potato_engaudio.mp3", image: "./assets/potato_photo.png"},
+                {id:7, chn:"黄瓜", eng:"cucumber", pinyin: "huáng guā", chnaudio: "./assets/cucumber_chnaudio.mp3", engaudio: "./assets/cucumber_engaudio.mp3", image: "./assets/cucumber_photo.png"},
+                {id:8, chn:"菜花", eng:"cauliflower", pinyin: "cài huā", chnaudio: "./assets/cauliflower_chnaudio.mp3", engaudio: "./assets/cauliflower_engaudio.mp3", image: "./assets/cauliflower_photo.png"},
+                {id:9, chn:"生菜", eng:"lettuce", pinyin: "shēng cài", chnaudio: "./assets/lettuce_chnaudio.mp3", engaudio: "./assets/lettuce_engaudio.mp3", image: "./assets/lettuce_photo.png"},
+                {id:10, chn:"洋葱", eng:"onion", pinyin: "yáng cōng", chnaudio: "./assets/onion_chnaudio.mp3", engaudio: "./assets/onion_engaudio.mp3", image: "./assets/onion_photo.png"},
             ],
             questions: [
                 //question, options, type, hint
                 //type can be chinese->english(1) or english->chinese(2)
                 //if there is no hint, that means player is the hinter
                 {q:1,o:[3,1,6,10], t:1, h:1},
-                {q:2,o:[1,5,2,4], t:1, s:2},
-                {q:3,o:[7,9,5,3], t:1, h:3},
-                {q:4,o:[4,10,8,1], t:1, s:4},
-                {q:5,o:[6,5,2,9], t:1, h:5},
-                {q:6,o:[7,10,3,6], t:1, s:6},
-                {q:7,o:[7,1,9,5], t:1, h:7},
-                {q:8,o:[10,8,3,4], t:1, s:8},
-                {q:9,o:[1,5,6,9], t:1, h:9},
-                {q:10,o:[10,7,3,4], t:1, s:10},
-    
-                {q:1,o:[3,1,6,10], t:1, s:1},
                 {q:2,o:[1,5,2,4], t:1, h:2},
-                {q:3,o:[7,9,5,3], t:1, s:3},
+                {q:3,o:[7,9,5,3], t:1, h:3},
                 {q:4,o:[4,10,8,1], t:1, h:4},
-                {q:5,o:[6,5,2,9], t:1, s:5},
+                {q:5,o:[6,5,2,9], t:1, h:5},
                 {q:6,o:[7,10,3,6], t:1, h:6},
-                {q:7,o:[7,1,9,5], t:1, s:7},
+                {q:7,o:[7,1,9,5], t:1, h:7},
                 {q:8,o:[10,8,3,4], t:1, h:8},
-                {q:9,o:[1,5,6,9], t:1, s:9},
+                {q:9,o:[1,5,6,9], t:1, h:9},
+                {q:10,o:[10,7,3,4], t:1, h:10},
+    
+                {q:1,o:[3,1,6,10], t:1, h:1},
+                {q:2,o:[1,5,2,4], t:1, h:2},
+                {q:3,o:[7,9,5,3], t:1, h:3},
+                {q:4,o:[4,10,8,1], t:1, h:4},
+                {q:5,o:[6,5,2,9], t:1, h:5},
+                {q:6,o:[7,10,3,6], t:1, h:6},
+                {q:7,o:[7,1,9,5], t:1, h:7},
+                {q:8,o:[10,8,3,4], t:1, h:8},
+                {q:9,o:[1,5,6,9], t:1, h:9},
                 {q:10,o:[10,7,3,4], t:1, h:10},
     
                 {q:1,o:[3,1,6,10], t:2, h:1},
-                {q:2,o:[1,5,2,4], t:2, s:2},
-                {q:3,o:[7,9,5,3], t:2, h:3},
-                {q:4,o:[4,10,8,1], t:2, s:4},
-                {q:5,o:[6,5,2,9], t:2, h:5},
-                {q:6,o:[7,10,3,6], t:2, s:6},
-                {q:7,o:[7,1,9,5], t:2, h:7},
-                {q:8,o:[10,8,3,4], t:2, s:8},
-                {q:9,o:[1,5,6,9], t:2, h:9},
-                {q:10,o:[10,7,3,4], t:2, s:10},
-    
-                {q:1,o:[3,1,6,10], t:2, s:1},
                 {q:2,o:[1,5,2,4], t:2, h:2},
-                {q:3,o:[7,9,5,3], t:2, s:3},
+                {q:3,o:[7,9,5,3], t:2, h:3},
                 {q:4,o:[4,10,8,1], t:2, h:4},
-                {q:5,o:[6,5,2,9], t:2, s:5},
+                {q:5,o:[6,5,2,9], t:2, h:5},
                 {q:6,o:[7,10,3,6], t:2, h:6},
-                {q:7,o:[7,1,9,5], t:2, s:7},
+                {q:7,o:[7,1,9,5], t:2, h:7},
                 {q:8,o:[10,8,3,4], t:2, h:8},
-                {q:9,o:[1,5,6,9], t:2, s:9},
+                {q:9,o:[1,5,6,9], t:2, h:9},
+                {q:10,o:[10,7,3,4], t:2, h:10},
+    
+                {q:1,o:[3,1,6,10], t:2, h:1},
+                {q:2,o:[1,5,2,4], t:2, h:2},
+                {q:3,o:[7,9,5,3], t:2, h:3},
+                {q:4,o:[4,10,8,1], t:2, h:4},
+                {q:5,o:[6,5,2,9], t:2, h:5},
+                {q:6,o:[7,10,3,6], t:2, h:6},
+                {q:7,o:[7,1,9,5], t:2, h:7},
+                {q:8,o:[10,8,3,4], t:2, h:8},
+                {q:9,o:[1,5,6,9], t:2, h:9},
                 {q:10,o:[10,7,3,4], t:2, h:10},
             ]
         }
@@ -170,6 +175,9 @@ Vue.component("quiz", {
         }
     },
     methods: {
+        play: function(id){
+            play(id);
+        },
         init: function(){
             this.setup(this.currentIndex);
         },
